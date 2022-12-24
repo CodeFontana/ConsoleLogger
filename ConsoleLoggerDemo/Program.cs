ed62ca3e4f6c8f2cf2c78c721b4c3cc7a67311ed
Brian Fontana <brian@codefoxtrot.com>
2022-12-24 17:32:34 -0500

    Update to .NET 7.0

--- ConsoleLoggerDemo/Program.cs
+++ ConsoleLoggerDemo/Program.cs
@@ -12,18 +12,7 @@ internal class Program
     {
         try
         {
-            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
-
             await Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(config =>
-                {
-                    config.SetBasePath(Directory.GetCurrentDirectory());
-                    config.AddJsonFile("appSettings.json", true, true);
-                    config.AddJsonFile($"appSettings.{env}.json", true, true);
-                    config.AddUserSecrets<Program>(optional: true);
-                    config.AddEnvironmentVariables();
-                })
                 .ConfigureLogging((context, builder) =>
                 {
                     builder.ClearProviders();
